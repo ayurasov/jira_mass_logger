f496d9579e7e5342793cdac6e6424c7de61956d2
Alexander Yurasov <ayurasov@gmail.com>
2026-08-17 23:46:04 +0300

    feat: register /my-worklog route for the editable worklog table screen

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'dashboard', component: () => import('../views/Dashboard.vue') },
+    { path: '/my-worklog', name: 'my-worklog', component: () => import('../views/MyWorklog.vue') },
     { path: '/bulk-log', name: 'bulk-log', component: () => import('../views/BulkLogWizard.vue') },
     { path: '/profiles', name: 'profiles', component: () => import('../views/Profiles.vue') },
     { path: '/templates', name: 'templates', component: () => import('../views/Templates.vue') },
