576e8b01a7c1042cb438f2dbcbd840cbab1948f9
Alexander Yurasov <ayurasov@gmail.com>
2026-08-17 21:50:44 +0300

    feat: add bulk log wizard route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'dashboard', component: () => import('../views/Dashboard.vue') },
+    { path: '/bulk-log', name: 'bulk-log', component: () => import('../views/BulkLogWizard.vue') },
     { path: '/profiles', name: 'profiles', component: () => import('../views/Profiles.vue') },
     { path: '/templates', name: 'templates', component: () => import('../views/Templates.vue') },
     { path: '/settings', name: 'settings', component: () => import('../views/Settings.vue') },
