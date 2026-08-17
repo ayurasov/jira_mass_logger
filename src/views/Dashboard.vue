aa8f424989e5902bdd20b9a2860934e7ff2d32ad
Alexander Yurasov <ayurasov@gmail.com>
2026-08-17 21:50:52 +0300

    feat: make bulk log wizard the main dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="dashboard">
-    <h1>JiraTime</h1>
-    <p>Массовое логирование времени в Jira</p>
-  </div>
+  <BulkLogWizard />
 </template>
+
+<script setup lang="ts">
+import BulkLogWizard from './BulkLogWizard.vue';
+</script>
